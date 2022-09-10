f29d922942ce0ab29a566ec79081d5a5f5414825
Maksym Horbachevsky <maximgorb@gmail.com>
2022-09-10 04:22:24 +0300

    Added conditional rendering, post removing.

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,21 @@ function App() {
         setPosts([...posts, newPost])
     }
 
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList posts={posts} title={"List of Posts"}/>
+            {posts.length
+                ?
+                <PostList remove={removePost} posts={posts} title={"List of Posts"}/>
+                :
+                <h1 style={{textAlign: "center"}}>
+                    Posts are not found!
+                </h1>
+            }
+
         </div>
     );
 }
