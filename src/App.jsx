92f2386cd131ff548be9f0bbd7582e8edc28b137
Maksym Horbachevsky <maximgorb@gmail.com>
2022-09-10 05:04:10 +0300

    Added search and useMemo(:(((((((((()

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import React, {useState} from "react"
+import React, {useMemo, useState} from "react"
 import "./styles/App.css"
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: "Javascript", body: "Javascript - a good code language."},
-        {id: 2, title: "Javascript 2", body: "Javascript - a good code language."},
-        {id: 3, title: "Javascript 3", body: "Javascript - a good code language."},
+        {id: 1, title: "bb", body: "Javascript - a bad code language."},
+        {id: 2, title: "aa", body: "Javascript - a nice code language."},
+        {id: 3, title: "cc", body: "Javascript - a good code language."},
     ]);
+    const [selectedSort, setSelectedSort] = useState("")
+    const [searchQuery, setSearchQuery] = useState("")
+
+    const sortedPosts = useMemo(() => {
+        if (selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts;
+    }, [selectedSort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+    }, [searchQuery, sortedPosts]);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -17,12 +32,33 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+    }
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            {posts.length
+            <hr style={{margin: "15px 0"}}/>
+            <div>
+                <MyInput
+                    placeholder={"Search..."}
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                />
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Sort"
+                    options={[
+                        {value: "title", name: 'Sort by Name'},
+                        {value: "body", name: 'Sort by Description'}
+                    ]}
+                />
+            </div>
+            {sortedAndSearchedPosts.length
                 ?
-                <PostList remove={removePost} posts={posts} title={"List of Posts"}/>
+                <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"List of Posts"}/>
                 :
                 <h1 style={{textAlign: "center"}}>
                     Posts are not found!
