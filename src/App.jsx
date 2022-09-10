5bdf266c5f56515954b1f6be63f468e35547026d
Maksym Horbachevsky <maximgorb@gmail.com>
2022-09-11 02:07:27 +0300

    Added modal window and animations to post

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./styles/App.css"
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -11,6 +13,7 @@ function App() {
         {id: 3, title: "cc", body: "Javascript - a good code language."},
     ]);
     const [filter, setFilter] = useState({sort:"",query:''})
+    const [modal, setModal] =useState(false)
 
     const sortedPosts = useMemo(() => {
         if (filter.sort) {
@@ -25,6 +28,7 @@ function App() {
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
@@ -33,18 +37,15 @@ function App() {
 
     return (
         <div className="App">
-            <PostForm create={createPost}/>
+            <MyButton style={{marginTop:"30px"}} onClick={() => setModal(true)}>
+                Create Post!
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
             <hr style={{margin: "15px 0"}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
-            {sortedAndSearchedPosts.length
-                ?
-                <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"List of Posts"}/>
-                :
-                <h1 style={{textAlign: "center"}}>
-                    Posts are not found!
-                </h1>
-            }
-
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"List of Posts"}/>
         </div>
     );
 }
