a966551276e7f2055cc79d47ec37d5b9e88268ae
Maksym Horbachevsky <maximgorb@gmail.com>
2022-09-15 11:18:49 +0300

    Added select to change the limit of loaded posts. Final version of the App.

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -11,13 +11,14 @@ import Loader from "../components/UI/Loader/Loader";
 import Pagination from "../components/UI/pagination/Pagination";
 import PostForm from "../components/PostForm";
 import {useObserver} from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 function Posts() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: "", query: ''})
     const [modal, setModal] = useState(false)
     const [totalPages, setTotalPages] = useState(0);
-    const [limit] = useState(10);
+    const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
     const lastElement = useRef();
 
@@ -37,7 +38,7 @@ function Posts() {
 
     useEffect(() => {
         fetchPosts(limit, page);
-    }, [page]);
+    }, [page, limit]);
 
 
     const createPost = (newPost) => {
@@ -67,6 +68,17 @@ function Posts() {
             {postError &&
                 <h1>Error happened. ${postError}</h1>
             }
+            <MySelect
+                value={limit}
+                onChange={value => setLimit(value)}
+                defaultValue={"Amount of elements on page"}
+                options={[
+                    {value:5, name : "5"},
+                    {value:10, name : "10"},
+                    {value:25, name : "25"},
+                    {value:-1, name : "Show all posts"},
+                ]}
+            />
             <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"List of Posts"}/>
             <div ref={lastElement} style={{height: 20, background: "red"}} />
             {isPostsLoading &&
